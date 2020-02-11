e78db067173704035fca01b622823fb65aeaffe1
Lauren Miggins <lauren.miggins@gmail.com>
2020-02-11 11:59:01 -0500

    code correction and final touches

--- main.js
+++ main.js
@@ -2,11 +2,12 @@ let submitAnswerButton;
 let answerInput;
 let currentQuestion;
 let response;
-let responseColor = "green";
+let responseColor = "pink";
 let heading; 
+let correctAnswers = 0;
 
 //quiz questions list
-const statements = [
+let statements = [
     {question: 'What is the name of Morty\'s crush?', answer: 'jessica'},
     {question: 'What is Rick\'s last name?', answer: 'sanchez'},
     {question: 'What does Beth do for a living?', answer: 'horse surgeon'},
@@ -22,7 +23,6 @@ currentQuestion = next();
 let message = currentQuestion.question;
 
 
-
 function checkQuestion() {
     if (currentQuestion.answer === answerInput.value()) {
         // remove correct answer from array
@@ -30,10 +30,13 @@ function checkQuestion() {
             return currentQuestion.answer !== statementsObj.answer;
         });
         // this is the correct condition
+        correctAnswers = correctAnswers + 1;
         response = 'correct!';
+        responseColor = '#d0fef5';
     } else {
         // this is the wrong answer condition
-        response = 'Wubba lubba dub dub! That was not correct. Please try again';
+        response = 'Wubba lubba dub dub! That was not correct. Please try again.';
+        responseColor = '#a10b08';
     }
     currentQuestion = next(); 
     answerInput.value('');
@@ -56,9 +59,10 @@ function setup() {
     background("#639986");
     // Quiz Title
     textFont("bely-display");
-    heading = createElement('h1', ['The Even More Stylish Rick & Morty Quiz!'])
-    heading.position(100, 75);
+    heading = createElement('h2', ['The Even More Stylish Rick & Morty Quiz!'])
+    heading.position(145, 75);
     // Input field
+    textFont("mr-eaves-xl-modern");
     answerInput = createInput('type answer here');
     answerInput.position(300, 350);
     answerInput.size(180, 30);
@@ -71,9 +75,20 @@ function setup() {
 }
 function draw() {
     background("#639986");
+    //line work
+    stroke("#d0fef5");
+    noFill();
+    quad(130, 65, 650, 65, 650, 130, 130, 130);
+    quad(20, 20, 780, 20, 780, 580, 20, 580);
+    quad(15, 15, 785, 15, 785, 585, 15, 585);
+    //text
+    noStroke();
     fill("#f7e54f");
-    textSize(20);
-    text(message, 40, 275);
+    textSize(25);
+    text(message, 120, 260);
     fill(responseColor); 
-    text(response, 100, 440);
+    text(response, 120, 480);
+    //Score Board
+    fill("pink");
+    text('You have ' + correctAnswers + ' of 9 correct answers needed to win!', 120, 520);
 }
